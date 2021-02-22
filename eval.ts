7fc0f33d8eb71ab6f7ef3bbc1b4776acef5d90ab
Jungle <jinshengjie@bilibili.com>
2021-02-22 16:56:03 +0800

    类型更新

--- eval.ts
+++ eval.ts
@@ -1,11 +1,11 @@
 import ASTNode from "./parser/ASTNode";
 import ASTNodeType from "./parser/ASTNodeType";
 
-const variables: {[K:string]: number} = {}
+const variables: { [K: string]: number } = {}
 
 const evaluate = (node: ASTNode) => {
-  let res: any
-  
+  let res: number
+
   switch (node.nodeType) {
     case ASTNodeType.Programm: {
       node.children.forEach(child => {
@@ -18,7 +18,7 @@ const evaluate = (node: ASTNode) => {
       const child2 = node.getChildren()[1]
       const value1 = evaluate(child1)
       const value2 = evaluate(child2)
-      return node.getText() === '+' 
+      return node.getText() === '+'
         ? value1 + value2
         : value1 - value2
     }
@@ -27,23 +27,23 @@ const evaluate = (node: ASTNode) => {
       const child2 = node.getChildren()[1]
       const value1 = evaluate(child1)
       const value2 = evaluate(child2)
-      return node.getText() === '*' 
+      return node.getText() === '*'
         ? value1 * value2
         : value1 / value2
     }
     case ASTNodeType.IntLiteral:
-        return Number(node.getText())
+      return Number(node.getText())
     case ASTNodeType.Identifier: {
       const varName = node.getText()
       if (variables.hasOwnProperty(varName)) {
         return variables[varName]
       } else {
-          throw new Error("未定义的变量 " + varName)
+        throw new Error("未定义的变量 " + varName)
       }
     }
     case ASTNodeType.AssignmentStmt:
       let varName = node.getText()
-      if (!variables.hasOwnProperty(varName)){
+      if (!variables.hasOwnProperty(varName)) {
         throw new Error("未定义的变量 " + varName)
       }   //接着执行下面的代码
     case ASTNodeType.IntDeclaration:
@@ -51,8 +51,8 @@ const evaluate = (node: ASTNode) => {
       let varValue
       // 存在赋值语句，由赋值语句走下来的话一定会有children
       if (node.getChildren().length) {
-          const child = node.getChildren()[0]
-          varValue = evaluate(child)
+        const child = node.getChildren()[0]
+        varValue = evaluate(child)
       }
       variables[varName] = varValue
       break
